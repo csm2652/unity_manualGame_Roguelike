b2ed5c9f9aa1a228f2637250e6d794ea6f64ccff
RisingJade <csm48173738@gmail.com>
2018-03-03 19:35:41 +0900

    upload unity Roguelike unity tutorial

---
+++ Assets/Scripts/Loarder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loarder : MonoBehaviour {
+
+	public GameObject gameManager;
+	// Use this for initialization
+	void Awake()
+	{
+		if (GameManager.instance == null)
+			Instantiate (gameManager);
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
